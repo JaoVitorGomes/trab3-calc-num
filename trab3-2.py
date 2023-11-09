2971c42c6dc83d2fc7083be748da203c4ee16ed6
GleydsonMussel <108836948+GleydsonMussel@users.noreply.github.com>
2023-11-09 20:55:02 -0300

    Ajeitando coisas

--- trab3-2.py
+++ trab3-2.py
@@ -1,29 +1,19 @@
 import numpy as np
-from sympy import diff, symbols
-
+import sympy as sp
+import metodos 
 
 #Problema 1 letra A
-def function(x):
-    return (1500/x)*((1 + x)**(20*12)  - 1) - 750000
 
-def Newton(f, df, x0, tolerance, n_max):
-    # initialize iteration variables
-    n = 0
-    c = x0
+# Defina a variável simbólica
+x = sp.symbols('x')
 
-    # iterate until convergence
-    while abs(f(c)) > tolerance and n < n_max:
-        c = c - f(c) / df.subs('x', c)
-        n += 1
+# Defina a função simbólica
+funcao = ((1500 * (1 + x)**240) - 1500) / 750000 - x
+derivada = sp.diff(funcao,x)
 
-    error = abs(f(c))
-    return c, error, n
+[taxa, erro, n_iteracoes] = metodos.Newton(funcao, derivada, 1, 1e-6, 1000)
 
-x = symbols('x')
-dfun = diff(function(x), x)
-
-root, error, n = Newton(function, dfun, 1, 1e-6, 100)
-print(f"O valor estimado da raiz da função é {root} com um erro de {error} em {n} iterações.")
+print(f"O valor estimado da raiz da função é {taxa} com um erro de {erro} em {n_iteracoes} iterações.")
 
 
 #Problema 1 letra B
