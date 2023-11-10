7550dc61f04d8523cf76e69454b1a63827a56d8f
GleydsonMussel <108836948+GleydsonMussel@users.noreply.github.com>
2023-11-09 21:05:46 -0300

    Implementando questão 1

--- trab3-2.py
+++ trab3-2.py
@@ -11,10 +11,9 @@ x = sp.symbols('x')
 funcao = ((1500 * (1 + x)**240) - 1500) / 750000 - x
 derivada = sp.diff(funcao,x)
 
-[taxa, erro, n_iteracoes] = metodos.Newton(funcao, derivada, 1, 1e-6, 1000)
+[taxa, erro, n_iteracoes] = metodos.Newton(funcao, derivada, 0.05, 1e-6, 1000)
 
 print(f"O valor estimado da raiz da função é {taxa} com um erro de {erro} em {n_iteracoes} iterações.")
 
-
 #Problema 1 letra B
-
+funcao = ((1500 * (1 + x)**240) - 1500) / 750000 - x
