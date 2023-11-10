d36e8812fe563e57d4dc00ca4f271a3160562cf6
GleydsonMussel <108836948+GleydsonMussel@users.noreply.github.com>
2023-11-10 13:32:04 -0300

    Ajeitando parte pratica

--- trab3-2.py
+++ trab3-2.py
@@ -2,9 +2,8 @@ import numpy as np
 import sympy as sp
 import metodos 
 
-#Problema 1 letra A
+#--------------Problema 1 letra A------------------
 
-# Defina a variável simbólica
 x = sp.symbols('x')
 
 # Defina a função simbólica
@@ -12,8 +11,30 @@ funcao = ((1500 * (1 + x)**240) - 1500) / 750000 - x
 derivada = sp.diff(funcao,x)
 
 [taxa, erro, n_iteracoes] = metodos.Newton(funcao, derivada, 0.05, 1e-6, 1000)
-
 print(f"O valor estimado da raiz da função é {taxa} com um erro de {erro} em {n_iteracoes} iterações.")
+print()
 
-#Problema 1 letra B
-funcao = ((1500 * (1 + x)**240) - 1500) / 750000 - x
+#--------------Problema 1 letra B-------------------
+
+funcao = ((1500 * (1 + ((sp.sin(x/2) + 1)**0.0001))**240) - 1500) / 750000 - ((sp.sin(x/2) + 1)**0.0001)
+derivada = sp.diff(funcao, x)
+
+[meses_necessarios, erro, n_iteracoes] = metodos.Newton(funcao, derivada, 0.05, 1e-6, 1000)
+print("Meses necessarios para ganhar o dinheiro necessario = "+str(meses_necessarios/12))
+print()
+
+#--------------Problema 2 letra A--------------------
+funcao = (0.9*x*sp.exp(-x/3) - 1)
+derivada = sp.diff(funcao,x)
+
+[tempo, erro, n_iteracoes] = metodos.Newton(funcao, derivada, 0.05, 1e-6, 1000)
+print(f"O Tempo estimado para atingir a concentração desejada {tempo} com um erro de {erro} em {n_iteracoes} iterações.")
+print()
+
+#--------------Problema 2 letra B--------------------
+funcao = (0.9*x*sp.exp(-x/3) - 1)
+derivada = sp.diff(funcao,x)
+
+[tempo, erro, n_iteracoes] = metodos.Newton(funcao, derivada, 0.05, 1e-6, 1000)
+print(f"O Tempo estimado para atingir a concentração desejada {tempo} com um erro de {erro} em {n_iteracoes} iterações.")
+print()
